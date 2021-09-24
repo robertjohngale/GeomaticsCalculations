4dd53911e9c8a8edc9243e25323c69882f8e9bfd
Robert <robertjohngale@gmail.com>
2021-09-24 10:45:47 -0600

    Revert "Added a methods"
    This reverts commit 61c232a45ca826a8886e5da132a1ca330e05159c.

--- main.py
+++ main.py
@@ -6,30 +6,8 @@
 # These might work for other datums/speroids but have not been tested
 
 import math
-def get_UTMconvergence(lat,lng,zone):
-
-    #a = 6378137; // semi - major axis for GRS 1980
-    e2 = 0.00669438 # 'eccentricity for GRS 1980
-    eccPrimeSquared = (e2) / (1 - e2)
-    utmConv = 0
-    latRad = lat * math.pi / 180
-    lngRad = lng * math.pi / 180
-
-    CM = (zone - 1) * 6 - 180 + 3
-    CMRad = CM * math.pi / 180
-
-    dlng = lngRad - CMRad
-    cosSqLat = math.pow(math.cos(latRad), 2)
-
-    etaSq = eccPrimeSquared * cosSqLat
-    utmConv = dlng * (1 + (math.pow(dlng, 2) / 3 * (1 + 3 * etaSq) * cosSqLat)) * math.sin(latRad);
-    utmConv = utmConv * (180 / math.pi);
-    return utmConv;
-
-
-
-def latlongto3TM(lat,lng):
-    return "NOT EMPLMENTED"
+def get_convergence():
+    return "NOT IMPLEMENTED YET"
 
 def get_combined_scale_factor(lat, lng, ellipsoid_height, a, e2, zone):
     k0 = 0.9996  # scale factor at the central meridian
@@ -58,34 +36,7 @@ def get_combined_scale_factor(lat, lng, ellipsoid_height, a, e2, zone):
 
     return scale_factor
 
-def getzonefromlongitude(lng):
-    lng = abs(lng)
-    zone = 0
-    if (lng <= 126 and lng > 120):
-        zone = 10
-    elif (lng <= 120 and lng > 114):
-        zone = 11
-    elif (lng <= 114 and lng > 108):
-        zone = 12
-    elif (lng <= 108 and lng > 102):
-        zone = 13
-    elif (lng <= 102 and lng > 96):
-        zone = 14
-    elif (lng <= 96 and lng > 90):
-        zone = 15
-    return zone
-
-
-
-        
 
 if __name__ == '__main__':
-    lng = -117.3454
-    lat = 55.234
-    zone = getzonefromlongitude(lng)
-    print (zone)
-    csf = get_combined_scale_factor(lat,lng,800,0,0,zone)
-    convergence = get_UTMconvergence(lat,lng,zone)
-    print (csf)
-    print (convergence)
+    csf = get_combined_scale_factor()
     print('\nProcess Complete')
